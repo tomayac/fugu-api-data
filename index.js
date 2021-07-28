73f16fa0c82b6a57df47646ca424bfa0009b455e
Thomas Steiner <tomac@google.com>
2021-07-28 17:42:55 +0200

    Add sparkline and Blink feature ID

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const SPREADHSEET_URL =
   'https://spreadsheets.google.com/feeds/cells/1ndxh6sl0fSOLHFlMdSiLPqmGPMDVJqkJFYnQ0Hsmhwo/2/public/full?alt=json';
 
 (async () => {
-  const text = await fetch(SPREADHSEET_URL)
+  const text = `{${await fetch(SPREADHSEET_URL)
     .then((response) => response.json())
-    .then((data) => data.feed.entry[0].content.$t);
+    .then((data) =>
+      data.feed.entry.map((entry) => entry.content.$t).join(''),
+    )}}`;
   const patterns = `const patterns = ${text}`;
   const patternsModule = `export default ${text}`;
   fs.writeFile('./patterns.js', patterns, { encoding: 'utf-8' });
