7bac983ed948c8c4cedff0a06e49ef6cda6e8046
Thomas Steiner <tomac@google.com>
2021-08-31 21:44:04 +0200

    Update to Sheets API v4

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const fs = require('fs/promises');
-const fetch = require('node-fetch');
+import fs from 'fs/promises';
+import fetch from 'node-fetch';
 
 const SPREADSHEET_URL =
-  'https://spreadsheets.google.com/feeds/cells/1ndxh6sl0fSOLHFlMdSiLPqmGPMDVJqkJFYnQ0Hsmhwo/2/public/full?alt=json';
+  'https://sheets.googleapis.com/v4/spreadsheets/1ndxh6sl0fSOLHFlMdSiLPqmGPMDVJqkJFYnQ0Hsmhwo/values/Sheet2?alt=json&key=AIzaSyCkROWBarEOJ9hQJggyrlUFulOFA4h6AW0';
 
 (async () => {
   const text = `{${await fetch(SPREADSHEET_URL)
     .then((response) => response.json())
-    .then((data) =>
-      data.feed.entry.map((entry) => entry.content.$t).join(''),
-    )}}`;
+    .then((data) => data.values.map((entry) => entry).join(''))}}`;
   const patterns = `const patterns = ${text}`;
   const patternsModule = `export default ${text}`;
   fs.writeFile('./patterns.js', patterns, { encoding: 'utf-8' });
